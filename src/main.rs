40ce3dedffe445450e4c69a4360cc25f795430a5
Jan Ciolek <jan.ciolek@nearone.org>
2024-08-08 23:29:53 +0000

    v2

--- src/main.rs
+++ src/main.rs
@@ -1,59 +1,45 @@
 //! Expected flow:
-//! 1. Start listening on a TCP port
-//! 2. Create a new system, and spawn a future which creates a client and does a request
-//! 3. The request hangs because the server doesn't respond
-//! 4. The system is stopped
-//! 5. Client is dropped and the destructor tries to spawn a future but it panics (doesn't happen :/)
+//! 1. Create a new system
+//! 2. Create a client - use custom connector and TLS so that disconnect_timeout is Some, otherwise ConnectionPool destructor won't do anything
+//! 3. Complete a request
+//! 4. The connection is returned to the ConnectionPool
+//! 5. Make the client live longer by spawning a future that captures it
+//! 6. The system is stopped
+//! 7. Client is dropped and ConnectionPool destructor spawns a task to close the pooled connection. This panics because the system is already stopped.
+//!
+//! In reality 7. happens before 6. and the panic doesn't occur.
 
-use std::io::Read;
 use std::time::Duration;
 
-use awc::Client;
+use awc::{Client, Connector};
 
-// Listen on a TCP port, accept one connection and read from it, but don't respond
-fn listen() {
-    let listener = std::net::TcpListener::bind("127.0.0.1:8320").unwrap();
-    println!("Started listening");
-    for stream_res in listener.incoming() {
-        if let Ok(mut stream) = stream_res {
-            let mut buf = [0; 1024];
-            while let Ok(r) = stream.read(&mut buf) {
-                if r > 0 {
-                    println!("Read {} bytes", r);
-                }
-            }
-            std::thread::sleep(Duration::from_secs(1000000));
-        }
-    }
-}
-
-fn run_sys() {
+fn main() {
     let sys = actix::System::new();
     sys.block_on(async {
-        actix::spawn(async move {
-            let client = Client::builder().timeout(Duration::from_secs(5)).finish();
-            let req = client.get("http://127.0.0.1:8320").send();
-            match req.await {
-                Ok(_) => {
-                    println!("OK!");
-                }
-                Err(e) => {
-                    println!("Error: {:?}", e);
-                }
+        let client = Client::builder()
+            .timeout(Duration::from_secs(5))
+            .connector(Connector::new().max_http_version(awc::http::Version::HTTP_11))
+            .finish();
+        let req = client.get("https://telemetry.nearone.org/nodes").send();
+        match req.await {
+            Ok(_) => {
+                println!("OK!");
             }
+            Err(e) => {
+                println!("Error: {:?}", e);
+            }
+        }
+        actix::spawn(async move {
+            let c: Client = client;
+            let _g = c.get("abcd");
+            println!("Client has size: {}", std::mem::size_of_val(&c));
+            actix::clock::sleep(Duration::from_secs(100000)).await;
         });
         actix::clock::sleep(Duration::from_secs(1)).await;
         actix::System::current().stop();
     });
     sys.run().unwrap();
     println!("Dropped system");
-}
 
-fn main() {
-    std::thread::spawn(listen);
     std::thread::sleep(Duration::from_secs(1));
-
-    run_sys();
-
-    std::thread::sleep(Duration::from_secs(10));
 }
